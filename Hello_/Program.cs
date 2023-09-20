62126222be340af19d4abd5b11c69eee5928c8e8
AnastasiiaAleks <chiffka@gmail.com>
2023-09-20 22:26:29 +0300

    программа для дочи

---
+++ Hello_/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("скажи мне свое имя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Приятно познакомиться, " + name + "!");
